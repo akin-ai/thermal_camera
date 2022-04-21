07dacdd2e9a2eefc4ec2bbb9971842e2e17830bc
stephenlowe64 <73762790+stephenlowe64@users.noreply.github.com>
2022-04-21 12:58:58 +1000

    Removed excess code

--- SerialReadDisplay.py
+++ SerialReadDisplay.py
@@ -2,12 +2,8 @@ import serial
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.animation as animation
-import struct
-import time
-
 
 fig = plt.figure()
-
 
 #Serial takes two parameters: serial device and baudrate
 ser = serial.Serial('COM6', 115200)
@@ -23,14 +19,10 @@ def animate(i):
     
     for i in range(x):
         for j in range(y): 
-            if i*x+j<700:
-                randomArray[i][j] = float(delimitedData[i*y+j]) 
+            randomArray[i][j] = float(delimitedData[i*y+j]) 
 
-    plt.imshow(randomArray, cmap='viridis', interpolation='nearest')
+    plt.imshow(randomArray, cmap='viridis', interpolation='none')
 
 
 ani = animation.FuncAnimation(fig, animate, interval=500)
 plt.show()
-
-
-
